de9463a874f4c85742788b9aa2e384626c4e853a
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-06-10 09:29:58 +0530

    Add simple class 1 programs

---
+++ simple/first.py
@@ -0,0 +1,12 @@
+def num(n):
+    if (n % 3 == 0)  and (n % 5 == 0):
+        print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)
+
+for i in range(1, 101):
+    num(i)
