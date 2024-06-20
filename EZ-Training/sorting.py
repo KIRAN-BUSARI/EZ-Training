78db15e5812d3fcda9560e143d76855982404c10
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-06-20 16:05:30 +0530

    Add sorting

--- EZ-Training/sorting.py
+++ EZ-Training/sorting.py
@@ -1,57 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 class node_data:
-    def __init__(self,Node,key):
-        self.node=Node
-        self.key=key
-        self.left=None
-        self.right=None
+    def __init__(self, Node, key):
+        self.node = Node
+        self.key = key
+
 def topview(root):
-    temp=node_data(root,0)
-    q=[temp]
-    q.append(None)
+    if root is None:
+        return
+    key_d = {}
+    q = [node_data(root, 0)]
+    while q:
+        curr = q.pop(0)
+        if curr.key not in key_d:
+            key_d[curr.key] = curr.node.value
+        if curr.node.left:
+            q.append(node_data(curr.node.left, curr.key - 1))
+        if curr.node.right:
+            q.append(node_data(curr.node.right, curr.key + 1))
     
-    key_d={}
-    
-    while len(q)!=0:
-        curr=q.pop(0)
-    
-        if curr==None:
-            if len(q)==0:
-                break
-            else:
-                print()
-                q.append(None)
-        else:
-            if curr.key not in key_d.keys():
-                key_d[curr.key]=curr.node.value
-            #print(curr.value,end='')
-            if curr.node.left!=None:
-                temp=node_data(curr.node.left,curr.key-1)
-                q.append(temp)
-            if curr.right!=None:
-                temp=node_data(curr.node.right,curr.key+1)
-                q.append(temp)
-        for i in sorted(d):
-            print(d[i].value)
-        print(key_d)
+    print(key_d)
+    for key in sorted(key_d):
+        print(key_d[key])
 
-if __name__=="__main__":
-    root = node(1)
-
-    root.left = node(2)
-    root.right = node(3)
-
-    root.left.left=node(4)
-    root.left.right=node(5)
-
-    root.right.left=node(6)
-    root.right.right=node(7)
+if __name__ == "__main__":
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.right.left.right = Node(8)
+    root.right.left.left = Node(9)
+    root.right.right.left = Node(10)
+    root.right.right.right = Node(11)
+    root.right.right.left.left = Node(12)
+    root.right.right.left.right = Node(13)
     
-    root.right.left.right=node(8)
-    root.right.left.left=node(9)
-    
-    root.right.right.left=node(10)
-    root.right.right.right=node(11)
-    
-    root.right.right.left.left=node(12)
-    root.right.right.left.right=node(13)
     topview(root)
